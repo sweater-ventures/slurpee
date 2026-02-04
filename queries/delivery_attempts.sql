73772cb952a70d3df75a6a2eb01076390a7ce87e
Jason Corbett <jcorbett@sweaterventures.com>
2026-02-04 16:31:20 -0700

    feat: US-008 - Write sqlc queries for delivery attempts

---
+++ queries/delivery_attempts.sql
@@ -0,0 +1,13 @@
+-- name: InsertDeliveryAttempt :one
+INSERT INTO delivery_attempts (id, event_id, subscriber_id, endpoint_url, attempted_at, request_headers, response_status_code, response_headers, response_body, status)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+RETURNING *;
+
+-- name: ListDeliveryAttemptsForEvent :many
+SELECT * FROM delivery_attempts WHERE event_id = $1 ORDER BY attempted_at;
+
+-- name: ListDeliveryAttemptsForSubscriber :many
+SELECT * FROM delivery_attempts WHERE subscriber_id = $1 ORDER BY attempted_at;
+
+-- name: UpdateDeliveryAttemptStatus :one
+UPDATE delivery_attempts SET status = $1, response_status_code = $2, response_headers = $3, response_body = $4 WHERE id = $5 RETURNING *;
